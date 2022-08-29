5877e61fd28ef17f19c418603b37d67bb6d8aa39
Roman <sheikhroman042@gmail.com>
2022-08-29 17:30:29 +0600

    all code

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import { Stack } from '@mui/material';
+import { categories } from '../utils/constains';
+const Sidebar = () => {
+  return (
+      <Stack>
+          
+    </Stack>
+  )
+}
+
+export default Sidebar
